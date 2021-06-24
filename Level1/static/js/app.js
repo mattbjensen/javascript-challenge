039370630271883b2c4a2811e68bb18250939543
Matthew Jensen <mjensen@cokergroup.com>
2021-06-23 23:30:33 -0400

    Update Level 1, add Level 2

--- Level1/static/js/app.js
+++ Level1/static/js/app.js
@@ -7,6 +7,7 @@ console.log( tableData);
 
 var tbody = d3.select( "tbody");
 
+// Loop to buld table
 data.forEach( function( sighting) {
     console.log( sighting);
     var row = tbody.append( "tr");
@@ -17,19 +18,22 @@ data.forEach( function( sighting) {
     });
 });
 
+// Search button click event
 var button = d3.select( "#filter-btn");
 
 button.on( "click", function( event){
     d3.event.preventDefault();
     tbody.html( "");
 
+// Define filters
 var inputDate = d3.select( "#datetime"); 
-var inputValue = inputDate.property( "value");
+var dateValue = inputDate.property( "value");
 
-var filter = tableData.filter( tableData => tableData.datetime === inputValue);
-filter.forEach( function( dateData){
+// Filter search results
+var filter = tableData.filter( tableData => tableData.datetime === dateValue);
+filter.forEach( function( dateSelect){
     var row = tbody.append( "tr");
-    Object.entries( dateData).forEach( function( [key, value]) {
+    Object.entries( dateSelect).forEach( function( [key, value]) {
     var cell = tbody.append( "td");
     cell.text( value);
         });
--- Level2/static/js/app.js
+++ Level2/static/js/app.js
@@ -7,6 +7,7 @@ console.log( tableData);
 
 var tbody = d3.select( "tbody");
 
+// Loop to buld table
 data.forEach( function( sighting) {
     console.log( sighting);
     var row = tbody.append( "tr");
@@ -17,21 +18,45 @@ data.forEach( function( sighting) {
     });
 });
 
+// Search button click event
 var button = d3.select( "#filter-btn");
 
 button.on( "click", function( event){
     d3.event.preventDefault();
     tbody.html( "");
 
-var inputDate = d3.select( "#datetime"); 
-var inputValue = inputDate.property( "value");
+    // Define filters
+    var inputDate = d3.select( "#datetime"); 
+    var dateValue = inputDate.property( "value");
+    var inputCity = d3.select("#city").property("value");
+    var inputState = d3.select("#state").property("value");
+    var inputCountry = d3.select("#country").property("value");
+    var inputShape = d3.select("#shape").property("value");
 
-var filter = tableData.filter( tableData => tableData.datetime === inputValue);
-filter.forEach( function( dateData){
-    var row = tbody.append( "tr");
-    Object.entries( dateData).forEach( function( [key, value]) {
-    var cell = tbody.append( "td");
-    cell.text( value);
+    // Filter search results
+    var filter = tableData
+
+    if ( dateValue) {
+        filter = filter.filter(row => row.datetime === dateValue);
+    }
+    if (inputCity) {
+        filter = filter.filter(row => row.city === inputCity);
+    }
+    if (inputState) {
+        filter = filter.filter(row => row.state === inputState);
+    }
+    if (inputCountry) {
+        filter = filter.filter(row => row.country === inputCountry);
+    }
+    if (inputShape) {
+        filter = filter.filter(row => row.shape === inputShape);
+    }
+
+        filter.forEach( function( filterSelect){
+            var row = tbody.append( "tr");
+            Object.entries( filterSelect).forEach( function( [key, value]) {
+            var cell = tbody.append( "td");
+            cell.text( value);
+            });
         });
-    });
 });
